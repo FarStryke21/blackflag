3110a3b2b892171a724bbae6741cfc7befe1fe16
Aman Chulawala <aman.chulawala@gmail.com>
2023-03-28 15:22:16 -0400

    updates for prediction model

--- realsense_stream/src/display_image.py
+++ realsense_stream/src/display_image.py
@@ -3,6 +3,7 @@
 import rospy
 import cv2
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import String
 from cv_bridge import CvBridge
 
 def callback(image_msg):
@@ -10,7 +11,7 @@ def callback(image_msg):
     bridge = CvBridge()
     image = bridge.imgmsg_to_cv2(image_msg, desired_encoding = 'passthrough')
     cv_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
+    
     cv2.imshow('Image', cv_image)
     cv2.waitKey(1)
 
